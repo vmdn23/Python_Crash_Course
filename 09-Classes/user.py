35f528d24e062ad24f0b25ead696622548ec3cfc
Victor Nguyen <victormdnguyen88@gmail.com>
2018-05-20 16:53:08 +0000

    remove admin class to make user only module

--- 09-Classes/user.py
+++ 09-Classes/user.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""A module that contain the classes: User, Privleges and Admin"""
+"""A class used for modeling users"""
 
 class User():
     """A class that represents a simple user profile"""
@@ -31,24 +31,3 @@ class User():
     def reset_login_attempts(self):
         """Reset login_attempts to 0."""
         self.login_attempts = 0
-
-class Admin(User):
-    """A user with administrative privileges."""
-
-    def __init__(self, first_name, last_name, username, email, location):
-        """Initializes Admin"""
-        super().__init__(first_name, last_name, username, email, location)
-
-        # Initializes an empty set of privileges
-        self.privileges = Privileges([])
-
-class Privileges():
-    """A class used to store an admin's privileges."""
-
-    def __init__(self, privileges):
-        self.privileges = privileges
-
-    def show_privileges(self):
-        """Display the privileges that the admin has."""
-        for privilege in self.privileges:
-            print("- " + privilege)
